f9d3fb2e922e3e86c66f724797e443f44b25ec7f
wesleydematos <wesleydematos3@gmail.com>
2023-06-09 15:04:35 -0300

    feat: create footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="footer">
+    <p>OzUsers &copy; 2023</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FooterComp",
+};
+</script>
+
+<style scoped>
+#footer {
+  height: 5rem;
+  background-color: #131313;
+  border-top: 4px solid #111;
+  color: #00d256;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
